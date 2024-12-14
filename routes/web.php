d6f97756adb1d7e3e5f4ef37d89b701e026e5fcb
IniFadhil <rffadhil07@gmail.com>
2024-12-14 23:58:12 +0700

    mengerjakan Laravel11

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Fadhil Rf', 'title' => 'About']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
